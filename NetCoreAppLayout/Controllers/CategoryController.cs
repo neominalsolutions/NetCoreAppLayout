f8c920386ac1c1394a660c7679a6cdf552253406
mert.alptekin <mert.alptekin@neominal.com>
2022-01-14 09:43:18 +0300

    Muhammed için

---
+++ NetCoreAppLayout/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreAppLayout.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreAppLayout.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("create-category")] // Attribute routing yani sayfa buradaki route ile açılsın demek
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("create-category")] // httppost olarak create-category ile sayfaya istek atılsın demek
+        // [Bind("Name","Description")] attribute ile bazen formdaki bazı alanların sunucudaki methoda taşınmasını istemeyiz böyle durumlarda ilgili propertyNameleri yazmayız. Yazdıklarımız sunucuya post edilir.
+        public IActionResult Create([Bind("Name","Description")] CategoryCreateInputModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                // model validasyondan geçiyorsa demek.
+                // veri tabanına kaydı gönder 
+            }
+
+            return View();
+        }
+    }
+}
---
+++ NetCoreAppLayout/Models/CategoryCreateInputModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreAppLayout.Models
+{
+    // Input model son kullanıcı end user tarafından viewdan doldurup gönderilen ve arka planda entity'e değerleri maplenen sınıflardır. Amacı doğrulanmış form bilgilerini içerisinde tutmaktır. Bu sebeple sadece propertylerden oluşur. Veri taşıma görevi görür. Ayrıcı bu model üzerinde validayon (veri doğrulama) operasyonları yapılır.Verileri veri tabanına göndermen önce entitylere maplemeden önce bu katmanı kullanırız. Validasyon işlemleri için DataAnnotations kullanırız. Daha kompleks yapılarda fluent validation da tercih edilebilir.
+    public class CategoryCreateInputModel
+    {
+        [Required(ErrorMessage ="Name alanı boş geçilemez")]
+        public string Name { get; set; }
+
+        [MaxLength(200, ErrorMessage ="En fazla 200 karakter uzunluğunda giriniz")]
+        public string Description { get; set; }
+
+    }
+}
