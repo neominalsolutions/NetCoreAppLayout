29ee7b2fe219e42c7a859a7997a575cd89ca7362
mert.alptekin <mert.alptekin@neominal.com>
2022-01-14 14:33:02 +0300

    TagHelpers

---
+++ NetCoreAppLayout/TagHelpers/AlertTagHelper.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreAppLayout.TagHelpers
+{
+    public static class BsColorClass
+    {
+        public const string Success = "success";
+        public const string Danger = "danger";
+        public const string Info = "info";
+        public const string Warning = "warning";
+    }
+
+    public static class BSElementType
+    {
+        public const string Alert = "alert";
+        public const string Button = "btn";
+
+    }
+
+    // <bs-alert color="BsColorClass.Danger" message="Dikkat!" />
+
+    [HtmlTargetElement("bs-alert", Attributes = "message, color")]
+    public class AlertTagHelper:TagHelper
+    {
+        public string Message { get; set; } // attributeden gelen değerler ise property'e bağlanır.
+        public string Color { get; set; }
+
+        // yani tag helper view'den çağırdığımızda çalışan tetiklen method.
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div"; // çıktıyı div
+            output.Attributes.Add("class", $"{BSElementType.Alert} {BSElementType.Alert}-{Color}");
+            output.Attributes.Add("role", BSElementType.Alert);
+            output.Content.SetContent(Message); //  A simple primary alert—check it out!
+
+            //<div class="alert alert-primary" role="alert">
+            //A simple primary alert—check it out!
+            //</div >
+
+            base.Process(context, output);
+        }
+
+    }
+}
---
+++ NetCoreAppLayout/TagHelpers/DivHighLightTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreAppLayout.TagHelpers
+{
+    // div elementine color ile gönderilen renege göre div elementinin rengini değiştiriyoruz.
+    // div elementine uygulanabilir bir attribute olmuş oldu
+    [HtmlTargetElement("div",Attributes ="color")]
+    public class DivHighLightTagHelper: TagHelper
+    {
+        public string Color { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.Add("class", $"text-{Color}");
+            base.Process(context, output);
+        }
+    }
+}
